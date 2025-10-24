ce3970a825f39aa9715fd007bea9628fcd0a146f
Libotean Bogdan <liboteanbogdan361@gmail.com>
2025-10-24 15:01:25 +0300

    Learned about laravel

---
+++ resources/views/Components/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CRM App</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+</head>
+    <body class="dark:bg-[#0a0a0a] text-amber-600 flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+        {{ $slot }} <!-- echivalent   <?php echo $slot ?> -->
+    </body>
+</html>
