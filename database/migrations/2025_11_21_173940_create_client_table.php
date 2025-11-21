a07beadd645081beeeab0c73b24ee2556c23c549
Libotean Bogdan <liboteanbogdan361@gmail.com>
2025-11-21 20:02:21 +0200

    BD clienti

---
+++ database/migrations/2025_11_21_173940_create_client_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('client', function (Blueprint $table) {
+            $table->id();
+            $table->foreignID('user_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('type');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->char('cnp', 13)->nullable()->unique();
+            $table->string('cui', 12)->nullable()->unique();
+            $table->boolean('tva_payer')->default(false);
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->string('country')->nullable();
+            $table->string('county')->nullable();
+            $table->string('locality')->nullable();
+            $table->string('address')->nullable();
+            $table->string('status')->default('activ');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('client');
+    }
+};
