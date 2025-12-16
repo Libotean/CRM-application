96f0e5dfb917e8b00a779058bf8ccd2ebbb72053
Libotean Bogdan <liboteanbogdan361@gmail.com>
2025-12-16 13:55:24 +0200

    Pagina detalii clienti (read-only)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminClientController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Auth;
@@ -33,28 +34,30 @@ Route::middleware('auth')->group(function () {
 
         Route::resource('users', UserController::class);
 
-        Route::resource('clients', \App\Http\Controllers\AdminClientController::class);
+        Route::resource('clients', AdminClientController::class);
 
         // lista utilizatori
-        Route::get('/users', [UserController::class, 'index'])->name('users.index');
+        // Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
-        // form adaugare utilizator
-        Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+        // // form adaugare utilizator
+        // Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 
-        // procesare si salvare utilizator
-        Route::post('/users', [UserController::class, 'store'])->name('users.store');
+        // // procesare si salvare utilizator
+        // Route::post('/users', [UserController::class, 'store'])->name('users.store');
 
-        // afisare detalii utilizator
-        Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+        // // afisare detalii utilizator
+        // Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
 
-        // form editare utilizator
-        Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+        // // form editare utilizator
+        // Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
 
-        // form actualizare utilizator
-        Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+        // // form actualizare utilizator
+        // Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
 
-        // stergere utilizator
-        Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+        // // stergere utilizator
+        // Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+
+        //Route::get('/clients/{client}', [AdminClientController::class, 'show'])->name('clients.show');
     });
     
     //routa pentru consilier
